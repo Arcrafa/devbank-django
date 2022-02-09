1aebfb7d9e4185eae9275f24a99c0b02c78bedbf
Rafael Maldonado <39253034+Arcrafa@users.noreply.github.com>
2022-02-09 17:45:03 -0500

    Update admin.py
    added list_display

--- interested/admin.py
+++ interested/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Interested)
+class InterestedAdmin(admin.ModelAdmin):
+        list_display = ('full_name', 'student_code', 'email','category')
+
+admin.site.register(Interested,InterestedAdmin)
